91f98b48b43adef05e1e10db178259e87d7b8fb2
Gianmattia <giamalfred@yahoo.it>
2021-03-20 17:08:07 +0100

    first commit

---
+++ Controllers/SmartTicketController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NFCTicketingWebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class SmartTicketController : ControllerBase
+    {
+        private readonly ILogger<SmartTicketController> _logger;
+
+        public SmartTicketController(ILogger<SmartTicketController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public string Get()
+        {                        
+            return "Ciao";
+        }
+    }
+}
---
+++ SmartyAuthentication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NFCTicketingWebAPI
+{
+    public class SmartyAuthentication : AuthorizationFilterAttribute
+    {
+
+    }
+}
